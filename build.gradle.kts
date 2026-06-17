791f2fc77f9724b1f1981215ceb2d3e7a4ac1c0d
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-06-17 22:20:39 +0300

    chore: setup development database

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,10 +19,14 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-flyway")
+	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("tools.jackson.dataformat:jackson-dataformat-csv")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testCompileOnly("org.projectlombok:lombok")
 	testAnnotationProcessor("org.projectlombok:lombok")
---
+++ src/main/resources/db/migration/V1_0__init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE measurements (
+    "time" timestamptz NOT NULL,
+    weight numeric(5, 2) NOT NULL,
+    height numeric(4, 1) NOT NULL,
+    bmi numeric(3, 1) NOT NULL,
+    fat_rate numeric(4, 2) NULL,
+    body_water_rate numeric(4, 2) NULL,
+    bone_mass numeric(4, 2) NULL,
+    metabolism numeric(5, 1) NULL,
+    muscle_rate numeric(4, 2) NULL,
+    visceral_fat numeric(3, 1) NULL,
+    CONSTRAINT measurements_pk PRIMARY KEY ("time")
+);
