aa8d7d1b11b25883fad2a7c98c18a92453eb0da7
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-06-21 11:22:13 +0300

    chore: add actuator health endpoints

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-flyway")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.flywaydb:flyway-database-postgresql")
 	implementation("tools.jackson.dataformat:jackson-dataformat-csv")
 	compileOnly("org.projectlombok:lombok")
