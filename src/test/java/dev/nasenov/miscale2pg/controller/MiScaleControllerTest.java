68513b10a02f7ae3898948827a0f74f78dc8d4a8
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-07-01 17:44:49 +0300

    feat: enable problem details response for bad requests

---
+++ src/test/java/dev/nasenov/miscale2pg/controller/MiScaleControllerTest.java
@@ -0,0 +1,38 @@
+package dev.nasenov.miscale2pg.controller;
+
+import dev.nasenov.miscale2pg.configuration.JacksonConfiguration;
+import dev.nasenov.miscale2pg.service.MiScaleService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+@WebMvcTest
+@Import(value = JacksonConfiguration.class)
+class MiScaleControllerTest {
+
+    @MockitoBean
+    MiScaleService miScaleService;
+
+    @Autowired
+    MockMvcTester mockMvcTester;
+
+    @Test
+    void shouldReturnBadRequestWhenNoFileIsUploaded() {
+        mockMvcTester.post()
+            .uri("/api/measurements")
+            .multipart()
+            .exchange()
+            .assertThat()
+            .hasStatus(HttpStatus.BAD_REQUEST)
+            .bodyJson()
+            .convertTo(ProblemDetail.class)
+            .extracting(ProblemDetail::getStatus, ProblemDetail::getDetail)
+            .containsExactly(HttpStatus.BAD_REQUEST.value(), "Required part 'file' is not present.");
+    }
+
+}
