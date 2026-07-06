e66b02aa82b6ea04aea020b640e64b166d9ba15a
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-07-06 12:42:02 +0300

    chore: fix warning

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,8 +34,6 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:testcontainers-postgresql")
-	testCompileOnly("org.projectlombok:lombok")
-	testAnnotationProcessor("org.projectlombok:lombok")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
