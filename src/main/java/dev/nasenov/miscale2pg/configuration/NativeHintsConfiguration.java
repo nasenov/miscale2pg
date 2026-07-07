fe995e79b52a5a2b9399cbbd4f5b9698b639823a
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-07-07 21:57:20 +0300

    fix: add native hint for measurement model

--- src/main/java/dev/nasenov/miscale2pg/configuration/NativeHintsConfiguration.java
+++ src/main/java/dev/nasenov/miscale2pg/configuration/NativeHintsConfiguration.java
@@ -1,10 +1,11 @@
 package dev.nasenov.miscale2pg.configuration;
 
 import dev.nasenov.miscale2pg.dto.MiScaleMeasurement;
+import dev.nasenov.miscale2pg.model.Measurement;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RegisterReflectionForBinding(MiScaleMeasurement.class)
+@RegisterReflectionForBinding({MiScaleMeasurement.class, Measurement.class})
 public class NativeHintsConfiguration {
 }
