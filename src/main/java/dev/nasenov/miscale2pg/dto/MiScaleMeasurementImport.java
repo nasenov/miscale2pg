0beb3daa05890984f95a69f7859ee86a955e080a
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-07-15 09:28:55 +0300

    chore: fix deprecated usage of @Valid

--- src/main/java/dev/nasenov/miscale2pg/dto/MiScaleMeasurementImport.java
+++ src/main/java/dev/nasenov/miscale2pg/dto/MiScaleMeasurementImport.java
@@ -3,7 +3,7 @@ package dev.nasenov.miscale2pg.dto;
 import jakarta.validation.Valid;
 import java.util.List;
 
-public record MiScaleMeasurementImport(@Valid List<MiScaleMeasurement> measurements) {
+public record MiScaleMeasurementImport(List<@Valid MiScaleMeasurement> measurements) {
 
   public static MiScaleMeasurementImport of(List<MiScaleMeasurement> measurements) {
     return new MiScaleMeasurementImport(measurements);
