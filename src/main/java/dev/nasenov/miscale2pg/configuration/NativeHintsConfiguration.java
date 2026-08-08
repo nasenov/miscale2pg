2ab2bbad9b718dc2195815c48b773d0febeb1c4b
Nikolay Asenov <10465782+nasenov@users.noreply.github.com>
2026-08-08 21:35:23 +0300

    fix: add hibernate validator runtime hints

--- src/main/java/dev/nasenov/miscale2pg/configuration/NativeHintsConfiguration.java
+++ src/main/java/dev/nasenov/miscale2pg/configuration/NativeHintsConfiguration.java
@@ -3,13 +3,40 @@ package dev.nasenov.miscale2pg.configuration;
 import dev.nasenov.miscale2pg.dto.MeasurementResponse;
 import dev.nasenov.miscale2pg.dto.MiScaleMeasurement;
 import dev.nasenov.miscale2pg.model.Measurement;
+import org.springframework.aot.hint.MemberCategory;
+import org.springframework.aot.hint.RuntimeHints;
+import org.springframework.aot.hint.RuntimeHintsRegistrar;
+import org.springframework.aot.hint.TypeReference;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportRuntimeHints;
 
 @Configuration
+@ImportRuntimeHints(NativeHintsConfiguration.HibernateValidatorRuntimeHints.class)
 @RegisterReflectionForBinding({
   MiScaleMeasurement.class,
   Measurement.class,
   MeasurementResponse.class
 })
-public class NativeHintsConfiguration {}
+public class NativeHintsConfiguration {
+
+  static class HibernateValidatorRuntimeHints implements RuntimeHintsRegistrar {
+
+    @Override
+    public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+      hints
+          .reflection()
+          .registerType(
+              TypeReference.of("org.hibernate.validator.internal.util.logging.Log_$logger"),
+              builder -> builder.withMembers(MemberCategory.INVOKE_DECLARED_CONSTRUCTORS));
+      hints
+          .reflection()
+          .registerType(
+              TypeReference.of("org.hibernate.validator.internal.util.logging.Messages_$bundle"),
+              builder ->
+                  builder
+                      .withMembers(MemberCategory.INVOKE_DECLARED_METHODS)
+                      .withField("INSTANCE"));
+    }
+  }
+}
